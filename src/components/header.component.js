dbb25573de1c558ea774ffbc8af668e8be3ff28f
Esa Riihinen <esa.riihinen@gmail.com>
2019-05-16 07:54:05 +0300

    header component modified

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Header = styled.div`
+  display: flex;
+  flex: 1 1 100%;
+  width: 70%;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  background-color: white;
+  border: none;
+  color: black;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+  height: 40px;
+  border-radius: 16px;
+  :focus {
+    outline: none;
+  }
+  :hover {
+    background-color: #f1f1f1;
+  }
+`;
+
 const SearchInput = styled.input`
   height: 2rem;
   width: 70%;
@@ -14,4 +41,10 @@ const SearchInput = styled.input`
     outline: none;
   }
 `;
-export default (props) => <SearchInput onChange={props.search} />;
+export default (props) => (
+  <Header>
+    <SearchInput onChange={props.search} />
+    <Button>Clear</Button>
+    <Button>Images only</Button>
+  </Header>
+);
