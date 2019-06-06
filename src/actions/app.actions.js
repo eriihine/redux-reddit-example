297038196c246f8c80936a271f9d94d1a153b543
Esa Riihinen <esa.riihinen@gmail.com>
2019-06-06 20:20:55 +0300

    Action refactoring

--- src/actions/app.actions.js
+++ src/actions/app.actions.js
@@ -1,12 +1,10 @@
 export const TYPES = {
-  APP_SET_IMAGE_FILTER: 'APP_SET_IMAGE_FILTER',
+  APP_SET_IMAGE_FILTER: "APP_SET_IMAGE_FILTER",
 };
 
-export const setFilterImages = () => {
-  return async (dispatch, getState) => {
-    dispatch({
-      type: TYPES.APP_SET_IMAGE_FILTER,
-      status: !getState().app.filterImages,
-    });
-  };
+export const setFilterImages = () => async (dispatch, getState) => {
+  dispatch({
+    type: TYPES.APP_SET_IMAGE_FILTER,
+    status: !getState().app.filterImages,
+  });
 };
--- src/actions/reddit.actions.js
+++ src/actions/reddit.actions.js
@@ -1,99 +1,94 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const TYPES = {
-  LOAD_SUB_REDDIT: 'LOAD_SUB_REDDIT',
-  SEARCH_REDDITS: 'SEARCH_REDDITS',
-  SET_BUSY: 'SET_BUSY',
-  CLEAR_REDDIT_STORE: 'CLEAR_REDDIT_STORE',
-  LOAD_MORE_REDDITS: 'LOAD_MORE_REDDITS',
+  LOAD_SUB_REDDIT: "LOAD_SUB_REDDIT",
+  SEARCH_REDDITS: "SEARCH_REDDITS",
+  SET_BUSY: "SET_BUSY",
+  CLEAR_REDDIT_STORE: "CLEAR_REDDIT_STORE",
+  LOAD_MORE_REDDITS: "LOAD_MORE_REDDITS",
 };
 
-export const setBusy = (isBusy) => {
-  return async (dispatch) => {
-    dispatch({
-      type: TYPES.SET_BUSY,
-      isBusy,
-    });
-  };
+export const setBusy = (isBusy) => async (dispatch) => {
+  dispatch({
+    type: TYPES.SET_BUSY,
+    isBusy,
+  });
 };
 
-export const clearRedditStore = () => {
-  return async (dispatch) => {
-    dispatch({
-      type: TYPES.CLEAR_REDDIT_STORE,
-    });
-  };
+export const clearRedditStore = () => async (dispatch) => {
+  dispatch({
+    type: TYPES.CLEAR_REDDIT_STORE,
+  });
 };
 
-export const searchReddits = (searchTerm = 'suomi') => {
-  return async (dispatch, getState) => {
-    let after,
-      children = [];
-    try {
-      dispatch(setBusy(true));
-      const response = await axios.get(
-        `https://www.reddit.com/search.json?q=${searchTerm}`,
-      );
-      children = response.data.data.children;
-      after = response.data.data.after;
-    } catch (error) {
-      console.log(error);
-    }
-    dispatch({
-      type: TYPES.SEARCH_REDDITS,
-      children,
-      after,
-      searchTerm,
-    });
-    dispatch(setBusy(false));
-  };
-};
-
-export const loadMoreReddits = () => {
-  // TODO: save / get search term from redux
-  return async (dispatch, getState) => {
-    const after = getState().reddits.after;
-    const searchTerm = getState().reddits.searchTerm;
-    let nextAfter = null,
-      children = [];
-    if (after) {
-      try {
-        const response = await axios.get(
-          `https://www.reddit.com/search.json?q=${searchTerm}&count=25&after=${after}`,
-        );
-        children = response.data.data.children;
-        nextAfter = response.data.data.after;
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    dispatch({
-      type: TYPES.LOAD_MORE_REDDITS,
-      children,
-      after: nextAfter,
-    });
-  };
-};
-
-export const getSubReddit = (subReddit = 'r/suomi') => {
-  return async (dispatch, getState) => {
-    let after,
-      children = [];
+export const searchReddits = (searchTerm = "suomi") => async (
+  dispatch,
+  getState,
+) => {
+  let after,
+    children = [];
+  try {
     dispatch(setBusy(true));
+    const response = await axios.get(
+      `https://www.reddit.com/search.json?q=${searchTerm}`,
+    );
+    children = response.data.data.children;
+    after = response.data.data.after;
+  } catch (error) {
+    console.log(error);
+  }
+  dispatch({
+    type: TYPES.SEARCH_REDDITS,
+    children,
+    after,
+    searchTerm,
+  });
+  dispatch(setBusy(false));
+};
+
+export const loadMoreReddits = () => async (dispatch, getState) => {
+  const after = getState().reddits.after;
+  const searchTerm = getState().reddits.searchTerm;
+  let nextAfter = null,
+    children = [];
+  if (after) {
     try {
       const response = await axios.get(
-        `https://www.reddit.com/${subReddit}.json`,
+        `https://www.reddit.com/search.json?q=${searchTerm}&count=25&after=${after}`,
       );
       children = response.data.data.children;
-      after = response.data.data.after;
+      nextAfter = response.data.data.after;
     } catch (error) {
       console.log(error);
     }
-    dispatch({
-      type: TYPES.LOAD_SUB_REDDIT,
-      children,
-      after,
-    });
-    dispatch(setBusy(false));
-  };
+  }
+  dispatch({
+    type: TYPES.LOAD_MORE_REDDITS,
+    children,
+    after: nextAfter,
+  });
+};
+
+export const getSubReddit = (subReddit = "r/suomi") => async (
+  dispatch,
+  getState,
+) => {
+  let after,
+    children = [];
+  dispatch(setBusy(true));
+  try {
+    const response = await axios.get(
+      `https://www.reddit.com/${subReddit}.json`,
+    );
+    children = response.data.data.children;
+    after = response.data.data.after;
+  } catch (error) {
+    console.log(error);
+  }
+  dispatch({
+    type: TYPES.LOAD_SUB_REDDIT,
+    children,
+    after,
+  });
+  dispatch(setBusy(false));
 };
